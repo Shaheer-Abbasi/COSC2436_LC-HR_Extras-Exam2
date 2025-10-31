8ef6422ae66e94d183d4104eed4c4d5552baa1ad
Shaheer Abbasi <abbasi.shaheer14@gmail.com>
2025-10-30 20:04:43 -0500

    Add files via upload

---
+++ queueusingstacks.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+
+int main() {
+    int q;
+    cin >> q;
+
+    stack<int> s1, s2; 
+
+    while (q--) {
+        int type;
+        cin >> type;
+
+        if (type == 1) {
+            int x;
+            cin >> x;
+            s1.push(x);
+        } 
+        else if (type == 2) {
+            if (s2.empty()) {
+                while (!s1.empty()) {
+                    s2.push(s1.top());
+                    s1.pop();
+                }
+            }
+            if (!s2.empty()) s2.pop();
+        } 
+        else if (type == 3) {
+            if (s2.empty()) {
+                while (!s1.empty()) {
+                    s2.push(s1.top());
+                    s1.pop();
+                }
+            }
+            if (!s2.empty()) cout << s2.top() << endl;
+        }
+    }
+
+    return 0;
+}
